5f8f667b31dddcd4f3f8919a5a34c620d76af626
agusdimarioUCC <2400520@ucc.edu.ar>
2026-02-04 11:48:35 -0300

    Add validation for Supabase environment variables
    - Update `server.ts` and `client.ts` to throw errors if Supabase URL or Anon Key are missing.
    - Replace non-null assertions with explicit checks.
    - Minor syntax update to cookie `setAll` method in `server.ts`.

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -3,24 +3,25 @@ import {cookies} from 'next/headers'
 
 export async function createClient() {
 	const cookieStore = await cookies()
-
+	const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+	const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+	if (!supabaseURL || !supabaseAnonKey) {
+		throw new Error('Missing Supabase environment variables')
+	}
 	return createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		supabaseURL,
+		supabaseAnonKey,
 		{
 			cookies: {
 				getAll() {
 					return cookieStore.getAll()
 				},
-				setAll(cookiesToSet) {
+				setAll: function (cookiesToSet) {
 					try {
 						cookiesToSet.forEach(({name, value, options}) =>
 							cookieStore.set(name, value, options)
 						)
 					} catch {
-						// The `setAll` method was called from a Server Component.
-						// This can be ignored if you have middleware refreshing
-						// user sessions.
 					}
 				},
 			},
